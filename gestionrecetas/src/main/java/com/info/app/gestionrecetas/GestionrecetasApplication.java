c2ce2e236b464ccbcb526f60b1feae8c6063a235
Carlos Jerez <ermac715@gmail.com>
2024-10-22 20:02:57 -0300

    Creando el Repositorio

---
+++ gestionrecetas/src/main/java/com/info/app/gestionrecetas/GestionrecetasApplication.java
@@ -0,0 +1,13 @@
+package com.info.app.gestionrecetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionrecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionrecetasApplication.class, args);
+	}
+
+}
